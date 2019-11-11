555de7158d7bb31803db6f0d872bf75ecb94b804
Issac Penn <pbbsignup@outlook.com>
2019-11-11 12:33:43 +0800

    Initial Commit

---
+++ Habits/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Habits
+//
+//  Created by PBB on 2019/11/11.
+//  Copyright © 2019 PBB. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
